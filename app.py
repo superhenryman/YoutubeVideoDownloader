1afbd603457c5a82bc10698645474212a019cdfd
superhenryman <121818148+superhenryman@users.noreply.github.com>
2024-12-31 12:21:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -41,6 +41,9 @@ def Download(todownload):
         'outtmpl': '%(title)s.%(ext)s',
         'noplaylist': True,
         'cookiefile': 'cookies.txt',
+        'headers': {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    },
     }
     try:
         with yt.YoutubeDL(ydl_opts) as ydl:
